2c2d18a5cf5065ba13ac48df46c497d93226e13e
Michael-Cool <michaelnatterer@gmx.net>
2025-10-21 12:03:22 +0200

    Added schemas.py

---
+++ src/api/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class CustomerData(BaseModel):
+    gender: str
+    SeniorCitizen: int
+    Partner: str
+    Dependents: str
+    tenure: int
+    PhoneService: str
+    MultipleLines: str
+    InternetService: str
+    OnlineSecurity: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    StreamingTV: str
+    StreamingMovies: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
+    MonthlyCharges: float
+    TotalCharges: float
+
+
+class ChurnPrediction(BaseModel):
+    churn_probability: float
+    churn_label: str
